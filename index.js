317dd0f258f15ad019a64de47969fac1a7e63487
Aibol Kussain <aibolikdev@gmail.com>
2017-12-04 23:50:23 +0300

    feat: Create babel plugin for removing console entries in JavaScript

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = function({ types: t }) {
+  return {
+    visitor: {
+      Identifier(path, state) {
+        if (path.node.name === 'console') {
+          if (path.parent.property &&
+            t.isIdentifier(path.parent.property) &&
+            (state.opts.exclude === undefined || path.parent.property &&
+              !state.opts.exclude.includes(path.parent.property.name))
+          ) {
+            console.log('Found needed log');
+            let node = path.findParent(path => path.isExpressionStatement());
+            if(node) {
+                node.remove();
+                path.skip();
+            }
+          }
+        }
+      }
+    }
+  }
+}
